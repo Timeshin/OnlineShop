da5b58a00c59b5f84f5a92c7f694e6011f8f03af
Maksim Timeshin <timeshin0485@gmail.com>
2021-11-16 02:07:53 +0300

    edit

--- src/App.js
+++ src/App.js
@@ -1,58 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React, { Component, lazy, Suspense } from 'react';
+import {
+  Route,
+  Switch,
+  withRouter
+} from "react-router-dom";
+import Header from './components/header/header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
+const ProductsList = lazy(() => import('./components/PLP/ProductsList'))
+
+class App extends Component {
+  render() {
+    return (
+      <Suspense fallback={<span>Loading...</span>}>
+        <Header/>
+        <Switch>
+          <Route path="/:name" component={ProductsList} />
+        </Switch>
+      </Suspense>
+    )
+  }
 }
 
-export default App;
+export default withRouter(App)
--- src/index.js
+++ src/index.js
@@ -2,20 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { store } from './app/store';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { store } from './store/store';
 import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import { Provider as UrqlProvider } from 'urql';
+import client from "./client"
 
 ReactDOM.render(
-  <React.StrictMode>
+  <UrqlProvider client={client}>
     <Provider store={store}>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </Provider>
-  </React.StrictMode>,
+  </UrqlProvider>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
